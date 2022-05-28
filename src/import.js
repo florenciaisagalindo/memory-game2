0a0ade71e450af1bf298955fb10d62f8c6293c06
Florencia Isa Galindo <florenciaisagalindo@192.168.1.11>
2022-05-28 13:24:29 -0300

    memory-game2

---
+++ src/import.js
@@ -0,0 +1,60 @@
+import src1 from './images/sub25/ansu-fati.jpeg';
+import src2 from './images/sub25/alphonso-davies.jpeg';
+import src3 from './images/sub25/christian-pulisic.jpeg';
+import src4 from './images/sub25/erling-haaland.jpeg';
+import src5 from './images/sub25/jordan-sancho.jpeg';
+import src6 from './images/sub25/kylian-mbappe.jpeg';
+
+
+const images = [
+    {
+        src: src1,
+        player: 'Ansu Fati'
+    },
+    {
+        src: src1,
+        player: 'Ansu Fati'
+    },
+    {
+        src: src2,
+        player: 'Alphonso Davies'
+    },
+    {
+        src: src2,
+        player: 'Alphonso Davies'
+    },
+    {
+        src: src3,
+        player: 'Christian Pulisic'
+    },
+    {
+        src: src3,
+        player: 'Christian Pulisic'
+    },
+    {
+        src: src4,
+        player: 'Erling Haaland'
+    },
+    {
+        src: src4,
+        player: 'Erling Haaland'
+    },
+    {
+        src: src5,
+        player: 'Jordan Sancho'
+    },
+    {
+        src: src5,
+        player: 'Jordan Sancho'
+    },
+    {
+        src: src6,
+        player: 'Kylian Mbape'
+    },
+    {
+        src: src6,
+        player: 'Kylian Mbape'
+    }
+];
+
+export { images }
